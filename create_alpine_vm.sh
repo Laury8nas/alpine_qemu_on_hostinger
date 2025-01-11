7f0d191d9d702bb25db0853f20dd1b1457b22e30
Laury8nas <pivoraslaurynas@gmail.com>
2025-01-11 19:40:48 +0200

    Adding scripts for creating and opening VM

---
+++ create_alpine_vm.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c78d6c68fd1aa62a19c85fe8a864699c8002901bbf7faa3fb8fd39d9a764be2
+size 2091
---
+++ open_alpine_vm.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca8fc8f38000db68b09214036d8a570a7ff3f2fea3e2f6869376bb4105780e83
+size 706
